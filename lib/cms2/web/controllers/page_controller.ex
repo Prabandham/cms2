3d1a7baa7817b0f7dd4f598279e41d198337e5ba
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2017-07-05 15:31:23 +0530

    some minor tweaks to include assets

--- lib/cms2/web/controllers/page_controller.ex
+++ lib/cms2/web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule Cms2.Web.PageController do
   use Cms2.Web, :controller
 
+  plug :put_layout, false
+
   def index(conn, %{"page" => [page_slug]}) do
     if page_slug do
 #         Find the page by the name / slug and send it
@@ -8,12 +10,11 @@ defmodule Cms2.Web.PageController do
   end
 
   def index(conn, %{"page" => []}) do
-    page = Cms2.Db.Page
-        |> Cms2.Repo.get_by(default: true, published: true)
-        |> Cms2.Repo.preload(:layout)
+    page = Cms2.Db.Page |> Cms2.Repo.get_by(default: true, published: true) |> Cms2.Repo.preload(:layout)
     page_content = page.content
+    layout = page.layout
     main_layout_template = Liquid.Template.parse(layout.content)
-    {:ok, rendered, _} = Liquid.Template.render(main_layout_template, %{"page.content" => page.content})
-    render(conn, "index.html", page: page)
+    {:ok, rendered, _} = Liquid.Template.render(main_layout_template, %{"page_content" => page.content, "page_title" => page.title})
+    render(conn, "index.html", page: rendered)
   end
 end
